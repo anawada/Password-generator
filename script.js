1e4efc1c7d4be38b358da00513f878238d8dd7c8
Ana Wada <anamitsuko@hotmail.com>
2023-01-10 12:06:22 +0000

    validating that at least one character type should be selected

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ let upperCasedCharacters = [
   "Z",
 ];
 
-// variable declarated in a global scope to be used for 2  != functions
+// variable declarated in a global scope to be used for 2  !== functions
 let passwordLength = 0;
 
 // Function to prompt user for password options
@@ -134,8 +134,8 @@ function getRandom(arr) {
 
   for (let i = 0; i < passwordLength; i++) {
 
-    let randomCharacter = Math.floor(Math.random()*arr.length)
-    generatedPassword += arr[randomCharacter]
+    let randomIndex = Math.floor(Math.random()*arr.length)
+    generatedPassword += arr[randomIndex]
     
   }
   console.log("generated password" + generatedPassword);
@@ -151,41 +151,37 @@ function generatePassword() {
 
   // created a variable to manipulate the object generated in getPasswordOptions()
   let options = getPasswordOptions();
-  
-  //if and else created acessing the object to check if the answer was true of false and concatenating the
-  //arrays in case true
 
-  if (options.lowerCase === true){
-    chosenCharacters = chosenCharacters.concat(lowerCasedCharacters);
-  }else {
-    console.log("nope");
-  }
-  if (options.uppercase === true){
-    chosenCharacters = chosenCharacters.concat(upperCasedCharacters);
-  }else {
-    console.log("nope");
-  }
-  if (options.specialCharactersOp === true){
-    chosenCharacters = chosenCharacters.concat(specialCharacters);
-  }else {
-    console.log("nope");
-  }
-  if (options.numeric === true){
-    chosenCharacters = chosenCharacters.concat(numericCharacters);
-  }else {
-    console.log("nope");
-  }
-  
-  console.log(chosenCharacters);
-
-  if(chosenCharacters === []) {
+  if(options.lowerCase === false && options.uppercase === false && options.specialCharactersOp === false && options.numeric === false){
+    alert("You need to choose at least one option! Let's try again!")
     getPasswordOptions();
-  }else { null; 
-  }
-  // Call getRandom(Arr) function to randomize and create a new array with the password
-  getRandom(chosenCharacters);
+  }else {
+    //conditionals created acessing the object to check if the answer was true of false and concatenating the arrays in case true
+    if (options.lowerCase === true){
+      chosenCharacters = chosenCharacters.concat(lowerCasedCharacters);
+    }else {
+      console.log("nope");
+    }
+    if (options.uppercase === true){
+      chosenCharacters = chosenCharacters.concat(upperCasedCharacters);
+    }else {
+      console.log("nope");
+    }
+    if (options.specialCharactersOp === true){
+      chosenCharacters = chosenCharacters.concat(specialCharacters);
+    }else {
+      console.log("nope");
+    }
+    if (options.numeric === true){
+      chosenCharacters = chosenCharacters.concat(numericCharacters);
+    }else {
+      console.log("nope");
+    }
+    console.log(chosenCharacters)
+    getRandom(chosenCharacters);
 
   return generatedPassword
+  }
 }
 
 
